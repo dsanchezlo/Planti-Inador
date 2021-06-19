57bf1effb620b50999bba28608704b56bbab8e99
dsanchezlo <44549545+dsanchezlo@users.noreply.github.com>
2021-06-19 11:03:30 -0500

    Update Sensor_Humedad.ino

--- WiFi/Sensor_Humedad.ino
+++ WiFi/Sensor_Humedad.ino
@@ -9,15 +9,16 @@ void loop() {
   valorA0 = analogRead(A0);
   int hum = (100 - valorA0*100/1024)*2;
 
-  if(hum < 10){
-    Serial.print("00");
-    Serial.print(hum);
-  }
-  else if(hum >= 10 && hum < 100){
-    Serial.print("0");
-    Serial.print(hum);
-  }
-  else Serial.print("100");
+    if(hum < 10){
+      Serial.print("00");
+      Serial.print(hum);
+    }
+    else if(hum >= 10 && hum < 100){
+      Serial.print("0");
+      Serial.print(hum);
+    }
+    else Serial.print("100");
+  
   Serial.print("34567");
   delay(1000);
 }
