aac725a59ff5a254a112b3ec47635df273175c63
jsilva09 <82488285+jsilva09@users.noreply.github.com>
2021-06-20 15:58:27 -0500

    Add files via upload

--- activar.v
+++ activar.v
@@ -3,7 +3,7 @@ module activar(humedad, regar);
 	input wire [11:0] humedad;
 	output wire regar;
 
-	assign regar = (humedad[7:4] <= 'd1 && humedad[11:8] < 1)? 1 : 0; // Humedad menor al 19%
+	assign regar = (humedad[7:4] <= 'd1 && humedad[11:8] < 1)? 1 : 0; // Humedad menor al 20 %
 	//assign regar = 1;
 
 endmodule
