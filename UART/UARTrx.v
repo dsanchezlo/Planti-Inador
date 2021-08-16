2e38d8b7f8831d5ada21e26bf9795b010e97ee95
jsilva09 <82488285+jsilva09@users.noreply.github.com>
2021-08-15 19:18:29 -0500

    Update UARTrx.v

--- UART/UARTrx.v
+++ UART/UARTrx.v
@@ -1,22 +1,22 @@
-//		Caracterí­sticas:
+//		Características:
 //		Velocidad de transmisión Baudios 115200
 //		Número de bits 8
 //		Bit de inicio 0
 
 module UARTrx(rx, clkBaud, dato, hecho, ERRrx);
-	input wire rx;												// Datos comunicacón serial
-	input wire clkBaud;										// Reloj para 115200 Baudios
-	output reg [7:0] dato;									// Dato leí­do
-	output reg hecho;											// Indica si ya se leyó el valor correcto
-	output reg ERRrx;											// Indica si hay algún fallo en el dato leído (Paridad)
+	input wire rx;										// Datos comunicacón serial
+	input wire clkBaud;									// Reloj para 115200 Baudios
+	output reg [7:0] dato;									// Dato leído
+	output reg hecho;									// Indica si ya se leyó el valor correcto
+	output reg ERRrx;									// Indica si hay algún fallo en el dato leído (Paridad)
 	
 	// Estados necesarios para recibir los datos
 	parameter ESPERAR = 2'b00;
 	parameter RECIBIR = 2'b01;
 	
-	reg [1:0] estado;											// Máquina de estados
-	reg recibido;												// Referencia al estado RECIBIR
-	reg [2:0] bitActual;										// Contador posición en la que se está leyendo
+	reg [1:0] estado;									// Máquina de estados
+	reg recibido;										// Referencia al estado RECIBIR
+	reg [2:0] bitActual;									// Contador posición en la que se está leyendo
 	
 	initial begin
 		estado = ESPERAR;
@@ -38,7 +38,7 @@ module UARTrx(rx, clkBaud, dato, hecho, ERRrx);
 	always @(posedge clkBaud) begin
 		case(estado)
 			ESPERAR: begin
-							if (!rx) begin						// Se activa con el primer bit de la señal (cero)
+							if (!rx) begin				// Se activa con el primer bit de la señal (cero)
 								hecho <= 1'b0;
 								ERRrx <= 1'b0;
 								bitActual <= 3'd0;
@@ -47,15 +47,15 @@ module UARTrx(rx, clkBaud, dato, hecho, ERRrx);
 							end
 						end
 			RECIBIR: begin
-							if(bitActual == 3'd7) begin	// Si ya se leyeron todos los bits
+							if(bitActual == 3'd7) begin		// Si ya se leyeron todos los bits
 								recibido <= 1'b0;
-								ERRrx <= ((rx && 				// Números que requieren que el bit de paridad sea 1
+								ERRrx <= ((rx && 		// Números que requieren que el bit de paridad sea 1
 									dato[6:0] != 7'd49 && 	// 1 en ASCII
 									dato[6:0] != 7'd50 && 	// 2 en ASCII
 									dato[6:0] != 7'd52 && 	// 4 en ASCII
 									dato[6:0] != 7'd55 &&	// 7 en ASCII
 									dato[6:0] != 7'd56) || 	// 8 en ASCII
-									(!rx && 						// Números que requieren que el bit de paridad sea 0
+									(!rx && 		// Números que requieren que el bit de paridad sea 0
 									dato[6:0] != 7'd48 && 	// 0 en ASCII
 									dato[6:0] != 7'd51 && 	// 3 en ASCII
 									dato[6:0] != 7'd53 && 	// 5 en ASCII
