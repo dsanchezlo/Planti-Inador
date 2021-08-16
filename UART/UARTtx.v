c56cf4a662f080c6226a253d12e3cd62a160089b
jsilva09 <82488285+jsilva09@users.noreply.github.com>
2021-08-15 19:19:13 -0500

    Update UARTtx.v

--- UART/UARTtx.v
+++ UART/UARTtx.v
@@ -1,17 +1,17 @@
 module UARTtx(init, clkBaud, datotx, tx);
-	input wire init;												// Empezar a enviar el byte
-	input wire clkBaud;											// Reloj para 115200 Baudios
+	input wire init;										// Empezar a enviar el byte
+	input wire clkBaud;										// Reloj para 115200 Baudios
 	input wire [7:0] datotx;									// Incluye el dato a enviar y el bit de paridad
-	output reg tx;													// Transmisión de datos
+	output reg tx;											// Transmisión de datos
 	
 	// Estados necesarios para enviar los datos
 	parameter INICIAR = 2'b00;
 	parameter ENVIAR = 2'b01;
 
-	reg [1:0] estado;												// Máquina de estados
-	reg inicio;														// Referencia al estado INICIAR
-	reg envio;														// Referencia al estado ENVIAR
-	reg [2:0] posicion;											// Contador posición en la que se está enviando
+	reg [1:0] estado;										// Máquina de estados
+	reg inicio;											// Referencia al estado INICIAR
+	reg envio;											// Referencia al estado ENVIAR
+	reg [2:0] posicion;										// Contador posición en la que se está enviando
 
 	initial begin
 		tx = 1'b1;
