b5147b8e1548887a542c43f18369aa035842da2b
jsilva09 <82488285+jsilva09@users.noreply.github.com>
2021-08-15 19:12:19 -0500

    Update Electrovalvula.v

--- Electroválvula/Electrovalvula.v
+++ Electroválvula/Electrovalvula.v
@@ -1,8 +1,8 @@
 module Electrovalvula(lowLevel, highLevel, MODelectrov, activarEV);
 	input wire lowLevel;		// Nivel de agua del tanque alto (5 %)
-	input wire highLevel;	// Nivel de agua del tanque alto (90 %)
+	input wire highLevel;		// Nivel de agua del tanque alto (90 %)
 	input wire MODelectrov;
-	output wire activarEV;	// Indica cuando se activa la electroválvula
+	output wire activarEV;		// Indica cuando se activa la electroválvula
 
 	wire alto;
 	wire bajo;
@@ -14,10 +14,10 @@ module Electrovalvula(lowLevel, highLevel, MODelectrov, activarEV);
 	//		lowLevel  == 0 cuando el nivel <  5 %
 	//		lowLevel  == 1 cuando el nivel >  5 %
 
-	//		if(highLevel && lowLevel)		tanque lleno, activarEV = 0 
-	//		if(highLevel && !lowLevel)		no ocurre porque el nivel no puede ser < 5 % y > 90 %
-	//		if(!highLevel && lowLevel)		si se está vaciando activarEV = 0, si se está llenando activarEV = 1
-	//		if(!highLevel && !lowLevel)	tanque vacío, activarEV = 1
+	//		if(highLevel && lowLevel)		Tanque lleno, activarEV = 0 
+	//		if(highLevel && !lowLevel)		No ocurre porque el nivel no puede ser < 5 % y > 90 %
+	//		if(!highLevel && lowLevel)		Si se está vaciando activarEV = 0, si se está llenando activarEV = 1
+	//		if(!highLevel && !lowLevel)		Tanque vacío, activarEV = 1
 
 
 	assign activarEV = (MODelectrov) ? bajo : 1'b0;
